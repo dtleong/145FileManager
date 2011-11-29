73c57f8c98c42984a2676ea7d746ba3f655c1485
Morgan Vigil <mvigil@mvigil.local>
2011-11-29 08:10:46 -0800

    Updated funciton signatures and added the file_directory struct.  Please enter the commit message for your changes. Lines starting

--- FileDirectory.c
+++ FileDirectory.c
@@ -1,14 +1,42 @@
 #include "FileDirectory.h"
 
-static FileInfo* fileTable[MAX_FILES];
+static FileInfo* file_table[MAX_FILES];
 
-void initFileInfo() {
+file_directory* f_dir_init(char* dir_name) {
+ 
+	struct file_directory* fdp;
 
-int i;
+	int i;
 
-for(i=0; i<MAX_FILES; i++) fileTable[i] = NULL;
+	for(i=0; i<MAX_FILES; i++) file_table[i] = NULL;
+	
+	struct file_directory fd = { dir_name, file_table };
+
+	fp = &fd;
+	return fp;
 }
 
-int fOpen(char* filename) {
-/* see board pseudo code */
-} 
+int f_mk_fs(struct file_directory* fd, char* filename) {
+/* This is where fcbs are initia;ized */
+}
+
+/* Open a file */
+int f_open(struct file_directory* fd, char* filename){
+/* Open a file named filename */
+}
+
+/* Close a file */
+int f_close(struct file_directory* fd, char* filename){
+
+}
+
+/* Create a new file */
+int f_create(struct file_directory* fd, char* filename){
+
+}
+
+/* Delete a file */
+int f_delete(struct file_directory* fd, char* filename){
+
+}
+ 
--- FileDirectory.h
+++ FileDirectory.h
@@ -3,6 +3,29 @@
 
 #define MAX_FILES 50
 
-void initFileDirectory();
+/* A file_directory struct definition */
+struct file_directory {
+	char* name;
+	FileInfo* file_table[MAX_FILES];
+};
 
-int fOpen(char* filename);
+/* Initialize a directory */
+void f_dir_init();
+
+/* Make a new file system */
+int f_mk_dir(struct file_directory* fd, char* filename);
+
+/* Open a file */
+int f_open(struct file_directory* fd, char* filename);
+
+/* Close a file */
+int f_close(struct file_directory* fd, char* filename);
+
+/* Create a new file */
+int f_create(struct file_directory* fd, char* filename);
+
+/* Delete a file */
+int f_delete(struct file_directory* fd, char* filename);
+
+/* Append a byte to the end of a file */
+int f_append_byte(struct file_directory* fd, int file_handle, 
