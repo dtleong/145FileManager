5aad7df006fb040218005dd9f5d815c127e6bf6f
Morgan Vigil <mvigil@mvigil.local>
2011-11-30 22:55:23 -0800

    Changed implementation of init_file_info(). Now a file_info_struct is initialized.

--- FileInfo.c
+++ FileInfo.c
@@ -1,22 +1,24 @@
 #include "FileInfo.h"
 #include <stdio.h>
 
-static char* name[NAME_SIZE]; 
-static FCB* fcb;
-static time_t created;
-static time_t modified;
+
+struct file_info* initFileInfo() {
+
+	char* name;
+	time_t created;
+	time_t modified;
+	struct data_block* db;
+
+	int i;
 
 
-void initFileInfo() {
 
-int i;
+	for(i=0; i<NAME_SIZE; i++) name[i] = NULL;
 
+	for(i=0; i<MAX_BLOCKS; i++) db[i] = NULL;
+	created = time(NULL);
+	modified = time(NULL);
 
-
-for(i=0; i<NAME_SIZE; i++) name[i] = NULL;
-
-fcb = NULL;
-created = time(NULL);
-modified = time(NULL);
+	struct file_info* fi = {created, modified, name, db}
 }
 
