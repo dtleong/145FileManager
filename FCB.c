a743cb7d4cfae3500d021c4153007c6224bf7c5d
Morgan Vigil <mvigil@mvigil.local>
2011-12-01 08:38:09 -0800

    Changed some typos and moved static struct to a local variable.

--- FCB.c
+++ FCB.c
@@ -1,14 +1,12 @@
 #include "FCB.h"
 
-static struct data_block* bList[MAX_BLOCKS];
-
 struct file_control_block* initFCB(){
-
+	struct data_block* bList[MAX_BLOCKS];
 	int i;
 
 	for(i=0; i<MAX_BLOCKS; i++) bList[i] = NULL;
 	
-	struct file_colntrol_block* fcb = {bList};
+	struct file_control_block* fcb = {bList};
 	return fcb;
 }
 
