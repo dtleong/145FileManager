5ad7d4eb74461fb878e0278a4f2e1c4211fb79dd
Morgan Vigil <mvigil@mvigil.local>
2011-11-30 22:57:15 -0800

    Updated the API for init_file_info().

--- FileInfo.h
+++ FileInfo.h
@@ -1,7 +1,14 @@
-#include "FCB.h"
+#include "FSCommon.h"
 #include <time.h>
 #include <stdio.h>
 
-#define NAME_SIZE 6
-
-void initFileInfo(); 
+/*  API for init_file_info()
+        purpose: Create a new file_info
+        parameters: [char* filename]
+        input preconditions: NONE
+        output/postconditions: [struct file_info* fi] A pointer to the new file_info struct is returned.
+        usage: struct file_info* init_file_info(char* filename)
+        Principal designer: mvigil
+        Status: Designed, documented, implemented
+*/
+struct file_info* init_file_info(char* filename); 
