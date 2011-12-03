f3160832f1e4091f6401d6892e57d70b8a2ac28f
dtleong <dtleong@hotmail.com>
2011-12-02 21:04:14 -0800

    API for FCB.c

--- FCB.c
+++ FCB.c
@@ -1,5 +1,15 @@
 #include "FCB.h"
 
+/*  API for init_fcb()
+	purpose: Initialize a new file control block
+	parameters: NONE
+	input preconditions: A file directory has been created, and f_open() or f_create has been called.
+	output/postsconditions: [struct file_control_block* fcbp] A pointer to the file control block is returned.
+	usage: init_fcb()  
+	Principal designer: daleong
+	Status: Designed, documented, implemented
+*/
+
 struct file_control_block* init_fcb() {
 	struct data_block* bList[MAX_BLOCKS];
 	int i;
