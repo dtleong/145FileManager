ba47e7d604cf5d3335866d68794fc8cc72b099dd
Morgan Vigil <mvigil@mvigil.local>
2011-12-03 09:30:46 -0800

    FileInfo.c has modify time function implemented.

--- FileInfo.c
+++ FileInfo.c
@@ -32,3 +32,35 @@ struct file_info* init_file_info(char *name) {
 	return fip;
 }
 
+/*  API for modify_file_info_time()
+        purpose: Updates a file_info last modified time to the current time
+        parameters: [struct file_info*]
+        input preconditions: file_info has been initialized. 
+        output/postconditions: NONE. The last modified time of file_info is equal to the current time.
+        usage: void modify_file_info_time(struct file_info* fi)
+        Principal designer: mvigil
+        Status: Designed, documented, implemented
+*/
+void  modify_file_info_time(struct file_info* fi) {
+	
+	time_t cur_time = time(NULL);
+	fi->last_modified = cur_time;
+	
+	return;
+}
+
+/* API for file_info_append_byte()
+        purpose: Appends a byte to the end of a file
+        parameters: [struct file_info* fi] [char* byte]
+        input preconditions: The file must not already be filled to max capacity.The file must be initialized.
+        output/postconditions: [int] Return number of bytes appended (1 or 0).
+        usage: int file_info_append_byte(struct file_info* fi, char* byte)
+        Principal designer: mvigil
+        Status: Designed, documented
+*/
+int file_info_append_byte(struct file_info* fi, char* byte) {
+	
+		
+
+}
+
